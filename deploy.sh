b98b3b23854ca206a050fa573716d6bb95badb4e
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2020-04-04 14:06:11 -0300

    Send docker information before script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-git tag -a $TAG -m "Creating tag version $TAG"
-git push origin $TAG
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker push brunokarpo/review-app:latest
 docker push brunokarpo/review-app:$TAG
