c8545b97c94ffde6dac993f48b50d71b6ac0428e
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2020-04-07 21:21:21 -0300

    #2 Loading and finishing database on test environment;

--- repository-jdbc/src/test/kotlin/nom/brunokarpo/review/repository/jdbc/ReviewRepositoryJdbcTest.kt
+++ repository-jdbc/src/test/kotlin/nom/brunokarpo/review/repository/jdbc/ReviewRepositoryJdbcTest.kt
@@ -1,9 +1,13 @@
 package nom.brunokarpo.review.repository.jdbc
 
 import nom.brunokarpo.review.core.model.Review
+import org.apache.ibatis.jdbc.ScriptRunner
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import java.io.BufferedReader
+import java.io.FileReader
 import java.sql.Types
 import java.util.*
 import kotlin.test.assertEquals
@@ -17,6 +21,10 @@ class ReviewRepositoryJdbcTest: DatabaseTestBase() {
     @BeforeEach
     internal fun setUp() {
         sut = ReviewRepositoryJdbc(DATA_SOURCE)
+
+        val scriptRunner = ScriptRunner(DATA_SOURCE.connection)
+        val reader = BufferedReader(FileReader(System.getProperty("user.dir") + "/src/test/resources/database/load-database.sql"))
+        scriptRunner.runScript(reader)
     }
 
     @Test
@@ -60,5 +68,33 @@ class ReviewRepositoryJdbcTest: DatabaseTestBase() {
         assertEquals(restId, rs.getObject("restaurant_id", UUID::class.java))
         assertEquals(5.0, rs.getDouble("average"))
         assertEquals(1, rs.getInt("qtd_review"))
+    }
+
+    @Test
+    internal fun `should recalculate review summary after create new review`() {
+        val restId = UUID.fromString("5c5749a5-e0e3-42a0-9d26-a428eb2e5344")
+        val orderId = UUID.randomUUID()
+        val userId = UUID.randomUUID()
+        val grade = 4
+
+        val review = Review(restaurantId = restId, userId = userId, orderId = orderId, review = grade)
+
+        sut.create(review)
+
+        val ps = DATA_SOURCE.connection.prepareStatement("SELECT restaurant_id, average, qtd_review FROM review_summary WHERE restaurant_id = ?")
+        ps.setObject(1, restId, Types.OTHER)
+
+        val rs = ps.executeQuery()
+        assertTrue { rs.next() }
+        assertEquals(restId, rs.getObject("restaurant_id", UUID::class.java))
+        assertEquals(4.4, rs.getDouble("average"))
+        assertEquals(5, rs.getInt("qtd_review"))
+    }
+
+    @AfterEach
+    internal fun tearDown() {
+        val scriptRunner = ScriptRunner(DATA_SOURCE.connection)
+        val reader = BufferedReader(FileReader(System.getProperty("user.dir") + "/src/test/resources/database/clean-database.sql"))
+        scriptRunner.runScript(reader)
     }
 }
