c56283377bcf65cc4255fb8fc4900a4f676b7b93
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2020-04-14 07:47:05 -0300

    #9 Created gateway method

--- repository-gateway/src/main/kotlin/nom/brunokarpo/review/gateway/repository/ReviewSummaryRepositoryGateway.kt
+++ repository-gateway/src/main/kotlin/nom/brunokarpo/review/gateway/repository/ReviewSummaryRepositoryGateway.kt
@@ -1,5 +1,6 @@
 package nom.brunokarpo.review.gateway.repository
 
+import nom.brunokarpo.review.core.model.Pageable
 import nom.brunokarpo.review.core.model.ReviewSummary
 import nom.brunokarpo.review.core.repository.ReviewSummaryRepository
 import java.util.*
@@ -8,4 +9,5 @@ interface ReviewSummaryRepositoryGateway: ReviewSummaryRepository {
 
     override fun getByRestaurantId(restaurantId: UUID): ReviewSummary?
 
+    override fun list(size: Int, page: Int): Pageable<ReviewSummary>
 }
