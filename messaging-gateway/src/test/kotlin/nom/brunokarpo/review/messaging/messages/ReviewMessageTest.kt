9f6d6265b241d7fb46b856a50fecede2b60a8c93
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2020-04-19 18:46:11 -0300

    #11 Created more tests

---
+++ messaging-gateway/src/test/kotlin/nom/brunokarpo/review/messaging/messages/ReviewMessageTest.kt
@@ -0,0 +1,52 @@
+package nom.brunokarpo.review.messaging.messages
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.random.Random
+import kotlin.test.assertEquals
+
+internal class ReviewMessageTest {
+
+    companion object {
+        val RESTAURANT_ID = UUID.randomUUID()!!
+        val ORDER_ID = UUID.randomUUID()!!
+        val USER_ID = UUID.randomUUID()!!
+        val REVIEW = Random.nextInt(1, 5)
+    }
+
+    private lateinit var sut: ReviewMessage
+
+    @BeforeEach
+    internal fun setUp() {
+        sut = ReviewMessage(restaurantId =  RESTAURANT_ID, orderId = ORDER_ID, userId = USER_ID, review = REVIEW)
+    }
+
+    @Test
+    internal fun `should create review model with restaurant id`() {
+        val result = sut.toModel()
+
+        assertEquals(RESTAURANT_ID, result.restaurantId)
+    }
+
+    @Test
+    internal fun `should create review model with user id`() {
+        val result = sut.toModel()
+
+        assertEquals(USER_ID, result.userId)
+    }
+
+    @Test
+    internal fun `should create review model with order id`() {
+        val result = sut.toModel()
+
+        assertEquals(ORDER_ID, result.orderId)
+    }
+
+    @Test
+    internal fun `should create review model with review`() {
+        val result = sut.toModel()
+
+        assertEquals(REVIEW, result.review)
+    }
+}
