efb222c2819ecda89c49475578330294a45df8f2
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2020-04-21 10:02:09 -0300

    #12 Created more publisher tests

--- messaging-spring-jms/src/test/kotlin/nom/brunokarpo/review/messaging/spring/jms/publishers/ReviewSummaryPublisherImplTest.kt
+++ messaging-spring-jms/src/test/kotlin/nom/brunokarpo/review/messaging/spring/jms/publishers/ReviewSummaryPublisherImplTest.kt
@@ -1,5 +1,6 @@
 package nom.brunokarpo.review.messaging.spring.jms.publishers
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import nom.brunokarpo.review.core.messaging.ReviewSummaryPublisher
 import nom.brunokarpo.review.core.model.ReviewSummary
 import nom.brunokarpo.review.messaging.spring.jms.MessagingTestBase
@@ -9,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import java.math.BigDecimal
 import java.util.*
 import kotlin.random.Random
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
+@Suppress("UNCHECKED_CAST")
 internal class ReviewSummaryPublisherImplTest: MessagingTestBase() {
 
     companion object {
@@ -21,6 +24,9 @@ internal class ReviewSummaryPublisherImplTest: MessagingTestBase() {
 
     @Autowired
     private lateinit var sut: ReviewSummaryPublisher
+
+    @Autowired
+    private lateinit var mapper: ObjectMapper
 
     private var message: Any? = null
 
@@ -40,4 +46,24 @@ internal class ReviewSummaryPublisherImplTest: MessagingTestBase() {
         assertNotNull(message)
     }
 
+    @Test
+    internal fun `should publish review with correct restaurant id`() {
+        val result: Map<String, Any> = mapper.readValue(message as String, Map::class.java) as Map<String, Any>
+
+        assertEquals(RESTAURANT_ID.toString(), result["restaurantId"])
+    }
+
+    @Test
+    internal fun `should publish review with correct average`() {
+        val result: Map<String, Any> = mapper.readValue(message as String, Map::class.java) as Map<String, Any>
+
+        assertEquals(AVERAGE.toDouble(), result["average"])
+    }
+
+    @Test
+    internal fun `should publish review with correct qtt review`() {
+        val result: Map<String, Any> = mapper.readValue(message as String, Map::class.java) as Map<String, Any>
+
+        assertEquals(QTD_REVIEW, result["qtdReview"])
+    }
 }
