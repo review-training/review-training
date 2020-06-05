7f44ae54d784884f1c35a6b194b03528414a5797
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2020-06-05 15:05:37 -0300

    #25 Enabling migrations with Flyway

--- quarkus-app-root/quarkus-app/src/test/kotlin/nom/brunokarpo/review/quarkus/resources/ReviewResourcesTest.kt
+++ quarkus-app-root/quarkus-app/src/test/kotlin/nom/brunokarpo/review/quarkus/resources/ReviewResourcesTest.kt
@@ -38,4 +38,33 @@ class ReviewResourcesTest {
                             "average", Matchers.equalTo(5.0f),
                             "qtdReview", Matchers.equalTo(1))
     }
+
+    @Test
+    internal fun `should create new review and recalculate summary`() {
+        val restId = UUID.fromString("b975588b-28e4-4bbd-b512-646da66e58ec")
+        val userId = UUID.randomUUID()!!
+        val orderId = UUID.randomUUID()!!
+        val review = 5
+        val body
+                = mapOf("restaurantId" to restId, "userId" to userId,
+                "orderId" to orderId, "review" to review)
+
+        given()
+            .request()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(body)
+            .`when`()
+                .post("/review")
+            .then()
+                .log().headers()
+                .and().log().body()
+                .and()
+                    .statusCode(201)
+                    .contentType(ContentType.JSON)
+                    .header("Location", Matchers.endsWith("/review?restaurant_id=$restId"))
+                    .body("restaurantId", Matchers.equalTo(restId.toString()),
+                            "average", Matchers.equalTo(2.3f),
+                            "qtdReview", Matchers.equalTo(25))
+    }
 }
