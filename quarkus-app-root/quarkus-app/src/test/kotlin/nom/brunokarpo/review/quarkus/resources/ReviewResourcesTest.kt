78753b48bb5e0e8331008ab1b7964ffa163ea3ee
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2020-06-11 20:07:26 -0300

    #25 Configuring activemq container

--- quarkus-app-root/quarkus-app/src/test/kotlin/nom/brunokarpo/review/quarkus/resources/ReviewResourcesTest.kt
+++ quarkus-app-root/quarkus-app/src/test/kotlin/nom/brunokarpo/review/quarkus/resources/ReviewResourcesTest.kt
@@ -1,15 +1,39 @@
 package nom.brunokarpo.review.quarkus.resources
 
+import com.github.dockerjava.api.model.ExposedPort
+import com.github.dockerjava.api.model.PortBinding
+import com.github.dockerjava.api.model.Ports
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType
 import org.hamcrest.Matchers
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import java.util.*
 
+class KGenericContainer(imageName: String) : GenericContainer<KGenericContainer>(imageName)
+
+@Testcontainers
 @QuarkusTest
 class ReviewResourcesTest {
+
+
+    companion object {
+        @Container
+        private val ACTIVEMQ_CONTAINER = KGenericContainer("vromero/activemq-artemis:latest-alpine")
+                .withExposedPorts(61616)
+                .withEnv("ARTEMIS_USERNAME", "admin")
+                .withEnv("ARTEMIS_PASSWORD", "admin")
+                // Below should not be used - Function is deprecated and for simplicity of test , You should override your properties at runtime
+                // By the way, Quarkus don't override properties at runtime
+                .withCreateContainerCmdModifier {
+                    it.withHostName("localhost")
+                            .withPortBindings(PortBinding(Ports.Binding.bindPort(61616), ExposedPort(61616)))
+                }
+    }
 
     @Test
     internal fun `should create new review`() {
